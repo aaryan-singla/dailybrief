b49adf3278205a340f18ada60ab0d35f23f74f6b
aaryan-singla <lostlikeboss@gmail.com>
2026-07-14 18:11:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,6 @@
 const DAILYBRIEF_CONFIG = {
   supabaseUrl:     'https://sgujvndpvhjvgotruwxd.supabase.co',       // e.g. https://xyzxyz.supabase.co
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNndWp2bmRwdmhqdmdvdHJ1d3hkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3ODY2NjgsImV4cCI6MjA5OTM2MjY2OH0.9Q4p7JBonoVqHDgaA4K77p4uL1vk_gQyHa1J6ZU28OU',  // starts with eyJ...
-  siteName:        'DailyBrief',
-  adminPassword:   'Thangos124!',        // for admin.html access
+  siteName:        'DailyBrief', 
   tagline:         'Business news. Read in minutes.',
 };
